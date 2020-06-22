cddfb9488c2e3321f622ac73b13201428036bb1d
Khalid Dhabbah <46940721+Dhabbah@users.noreply.github.com>
2020-06-22 13:50:08 -0400

    All works

---
+++ Lab2/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+@Component({
+  selector: 'app-my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'to Lab2';
+  state = 'MO';
+  city = 'Kansas City';
+  result: any;
+  result2: any;
+  constructor(private http: HttpClient) {
+  }
+  show() {
+    this.http.get('https://api.wunderground.com/api/4bbbc25f4f5946dd/conditions/q/' + this.state + '/' + this.city + '.json')
+      .subscribe((result) => {
+        this.result = result;
+        console.log(this.result);
+
+      });
+    this.http.get('https://api.wunderground.com/api/36b799dc821d5836/hourly/q/' + this.state + '/' + this.city + '.json')
+      .subscribe((result2) => {
+        this.result2 = result2;
+        console.log(this.result2);
+
+      });
+
+  }
+}
+
